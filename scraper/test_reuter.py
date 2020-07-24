b01e105a200f8acf1eb4578e29e7667341802c90
DESKTOP-JRAJVBN\anish <anishs1882001@gmail.com>
2020-07-24 17:46:13 -0400

    implemented infinite scroll

--- scraper/test_reuter.py
+++ scraper/test_reuter.py
@@ -1,30 +1,67 @@
+import time
+
 from selenium import webdriver
 
-DRIVER_PATH = './chromedriver_win32/chromedriver'
+
+def scroll(driver, timeout):
+    scroll_pause_time = timeout
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(scroll_pause_time)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            # If heights are the same it will exit the function
+            break
+        last_height = new_height
+
+
+DRIVER_PATH = "D:/talks-of-stocks/scraper/chromedriver_win32/chromedriver.exe"
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+
+# Wait before throwing an exception
+driver.implicitly_wait(30)
 
 # Go to Reuters AAPL newsfeed
 driver.get("https://www.reuters.com/companies/AAPL.OQ/news")
+scroll(driver, 5)
 
 # Get all text within the article listing
-article_listing = driver.find_element_by_xpath("//*[@id='__next']/div/div[4]/div[1]/div/div/div/div[2]/div[1]/div")
+# article_listing = driver.find_element_by_xpath("//*[@id='__next']/div/div[4]/div[1]/div/div/div/div[2]/div[1]/div")
 
-# Split text into headline and first sentence
-article_listing_components = article_listing.text.split("\n")
-headline = article_listing_components[0]
-sentence = article_listing_components[1]
+# Store all the article listings
+article_listings = []
+article_listings = driver.find_elements_by_class_name("MarketStoryItem-container-3rpwz")
 
-# Obtain the article for the current listing
-article = driver.find_element_by_link_text(headline)
-article_link = article.get_attribute("href")
+# Retrieve data from each article listing
+for article_listing in article_listings:
+    # Split text into headline and first sentence
+    article_listing_components = article_listing.text.split("\n")
+    headline = article_listing_components[0]
+    sentence = article_listing_components[1]
 
-# Go to current news article
-driver.get(article_link)
+    # Obtain the article for the current listing
+    # article = driver.find_element_by_link_text(headline)
+    # article_link = article.get_attribute("href")
 
-# Retrieve date from the article
-date = driver.find_element_by_class_name("ArticleHeader_date").text[0:13]
+    # Go to current news article
+    # driver.get(article_link)
 
-# Headline, first sentence, date
-print(headline)
-print(sentence)
-print(date)
+    # Retrieve date from the article
+    # date = driver.find_element_by_class_name("ArticleHeader_date").text[0:13]
+
+    # Go back to the main article listing page
+    # driver.back()
+
+    # Headline, first sentence, date
+    print(headline)
+    print(sentence)
+    # print(date)
